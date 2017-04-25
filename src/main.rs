99381729345cb72f76700d04b1d3d9b7759a3696
Ross Harrison <rtharrison86@gmail.com>
2017-04-25 08:37:18 -0400

    working on filling

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,140 @@
-use std::fmt;
+use std::collections::HashSet;
+use std::iter::Map;
 
-fn main() {
-    for num in 1i64..100 {
-        if div_by_three(num) {
-            print!("Fizz");
+#[derive(Hash, Eq, PartialEq, PartialOrd)]
+struct Coord {
+    i: usize,
+    j: usize
+}
+
+struct SudokuCell {
+    position: Coord,
+    value: u64,
+    neighbors: Vec<Coord>
+}
+
+impl SudokuCell {
+    fn new(position: Coord) -> SudokuCell {
+        let neighbors = SudokuCell::generateAllNeighbors(&position);
+
+        SudokuCell {
+            value: 0,
+            neighbors: neighbors,
+            position: position
         }
-        if div_by_five(num) {
-            print!("Buzz");
+    }
+
+    fn generateAllNeighbors(position: &Coord) -> Vec<Coord> {
+        let mut neighbors = HashSet::new();
+
+        for index in 0..SIZE {
+            neighbors.insert(Coord{i: index, j: position.j});
+            neighbors.insert(Coord{i: position.i, j: index});
         }
 
-        print!("- {}\n", num);
+        let iFloor = (position.i / THIRD) * THIRD;
+        let jFloor = (position.j / THIRD) * THIRD;
+
+        for i in iFloor..(iFloor + THIRD) {
+            for j in jFloor..(jFloor + THIRD) {
+                neighbors.insert(Coord{i: i, j: j});
+            }
+        }
+
+        return neighbors.into_iter().collect();
+    }
+}
+
+const FULL_SIZE : usize = 81;
+const SIZE : usize = 9;
+const THIRD: usize = 3;
+
+fn index_to_coord(index: usize) -> Coord {
+    Coord{
+        i: index / SIZE,
+        j: index % SIZE
+    }
+}
+
+#[test]
+fn test_index_to_coord (){
+   assert!(index_to_coord(0) == Coord{i: 0, j: 0});
+   assert!(index_to_coord(80) == Coord{i: 8, j: 8});
+}
+
+fn coord_to_index(coord: Coord) -> usize {
+    return coord.i * SIZE + coord.j;
+}
+
+fn coord_ref_to_index(coord: &Coord) -> usize {
+    return coord.i * SIZE + coord.j;
+}
+
+#[test]
+fn test_coord_to_index (){
+    assert!(coord_to_index(Coord{i: 0, j: 0}) == 0);
+    assert!(coord_to_index(Coord{i: 8, j: 8}) == 80);
+}
+
+struct SudokuBoard {
+   cells: Vec<SudokuCell>
+}
+
+impl SudokuBoard {
+    fn new() -> SudokuBoard {
+        let mut cells = Vec::with_capacity(81);
+        for index in 0..FULL_SIZE {
+            cells.push(SudokuCell::new(index_to_coord(index)));
+        }
+
+        SudokuBoard{ cells: cells}
+    }
+
+    fn print(&self) {
+        println!("----------------------------------");
+
+        for cell in &self.cells {
+            if cell.position.j == 0 {
+                print ! ("| ");
+            }
+            print!("{}  ", cell.value);
+
+            if (cell.position.j % THIRD) == (THIRD - 1) {
+                print ! ("| ");
+
+            }
+            if cell.position.j == SIZE -1 {
+                print!("\n");
+
+                if cell.position.i % THIRD == THIRD - 1 {
+                    println!("----------------------------------");
+                }
+            }
+        }
+    }
+
+    fn markCell(&mut self) {
+        let cells = self.cells.as_mut_slice();
+        let (head, tails) = cells.split_at_mut(60);
+        let mut cell = head.last_mut().unwrap();
+        cell.value = 1;
+
+        for n in cell.neighbors {
+            let neighbor = cells.get_mut(coord_to_index(n));
+            match neighbor {
+                Some(n) => n.value = 7,
+                None => println!("Cannot divide by 0")
+            }
+        }
+
+
     }
 }
 
 
-fn div_by_three(n: i64) -> bool {
-    n % 3 == 0
-}
 
-#[test]
-fn test_div_by_three() {
-    assert!(!div_by_three(1), "One is not three");
-    assert!(div_by_three(3), "three is three");
-}
-
-fn div_by_five(n: i64) -> bool {
-    n % 5 == 0
-}
-
-#[test]
-fn test_div_by_five() {
-    assert!(!div_by_five(1), "One is not five");
-    assert!(!div_by_five(3), "three is not five");
-    assert!(div_by_five(5), "five is five");
-    assert!(div_by_five(15), "fifteen is five");
+fn main() {
+    let mut board = SudokuBoard::new();
+    board.markCell();
+    board.print();
 }
