c3328944801bcf832aec76a7dccd65610834a7ea
Ross Harrison <rtharrison86@gmail.com>
2017-04-25 22:02:19 -0400

    option for optimal neighbors

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ extern crate rand;
 use std::collections::HashSet;
 use rand::{thread_rng, Rng};
 
+
+const FULL_SIZE : usize = 81;
+const SIZE : usize = 9;
+const THIRD: usize = 3;
+
+
 #[derive(Clone, Hash, Eq, PartialEq, PartialOrd)]
 struct Coord {
     i: usize,
@@ -11,13 +17,18 @@ struct Coord {
 
 struct SudokuCell {
     position: Coord,
-    value: u64,
+    value: u8,
     neighbors: Vec<Coord>
 }
 
 impl SudokuCell {
-    fn new(position: Coord) -> SudokuCell {
-        let neighbors = SudokuCell::generate_all_neighbors(&position);
+    fn new(position: Coord, all_neighbors: bool) -> SudokuCell {
+        let neighbors = if all_neighbors {
+            SudokuCell::generate_all_neighbors(&position)
+        }
+        else {
+            SudokuCell::generate_optimal_neighbors(&position)
+        };
 
         SudokuCell {
             value: 0,
@@ -45,11 +56,35 @@ impl SudokuCell {
 
         return neighbors.into_iter().collect();
     }
-}
 
-const FULL_SIZE : usize = 81;
-const SIZE : usize = 9;
-const THIRD: usize = 3;
+    fn generate_optimal_neighbors(position: &Coord) -> Vec<Coord> {
+        let mut neighbors = HashSet::new();
+
+        for index in 0..position.i {
+            neighbors.insert(Coord{i: index, j: position.j});
+        }
+
+        for index in 0..position.j {
+            neighbors.insert(Coord{i: position.i, j: index});
+        }
+
+        let i_floor = (position.i / THIRD) * THIRD;
+        let j_floor = (position.j / THIRD) * THIRD;
+
+
+        for i in i_floor..position.i {
+            for j in j_floor..(j_floor + THIRD) {
+                if i < position.i || j < position.j {
+                    neighbors.insert(Coord{i: i, j: j});
+                }
+
+            }
+        }
+        
+        return neighbors.into_iter().collect();
+    }
+
+}
 
 fn index_to_coord(index: usize) -> Coord {
     Coord{
@@ -76,17 +111,17 @@ fn test_coord_to_index (){
 
 struct SudokuBoard {
    cells: Vec<SudokuCell>,
-   valid_values: HashSet<u64>
+   valid_values: HashSet<u8>
 }
 
 impl SudokuBoard {
-    fn new() -> SudokuBoard {
+    fn new(all_neighbors: bool) -> SudokuBoard {
         let mut cells = Vec::with_capacity(81);
         for index in 0..FULL_SIZE {
-            cells.push(SudokuCell::new(index_to_coord(index)));
+            cells.push(SudokuCell::new(index_to_coord(index), all_neighbors));
         }
 
-        let valid_values : HashSet<u64> = vec!(1, 2, 3, 4, 5, 6, 7, 8, 9).into_iter().collect();
+        let valid_values : HashSet<u8> = vec!(1, 2, 3, 4, 5, 6, 7, 8, 9).into_iter().collect();
 
         SudokuBoard{ cells: cells, valid_values: valid_values}
     }
@@ -100,7 +135,7 @@ impl SudokuBoard {
         let neighbor_values = self.neighbor_values(cell_pos);
 
         // get remaining values and shuffle them
-        let mut remaining_values: Vec<u64> = self.valid_values
+        let mut remaining_values: Vec<u8> = self.valid_values
                     .difference(&neighbor_values).cloned().collect();
         let mut rng = rand::thread_rng();
         rng.shuffle(&mut remaining_values);
@@ -141,8 +176,28 @@ impl SudokuBoard {
         }
     }
 
-    fn mark_cell(&mut self, coord: &Coord, value: u64) {
+    fn clear(&mut self) {
+        for n in 0..FULL_SIZE {
+            self.mark_cell_pos(n, 0);
+        }
+    }
+
+    fn serialize(&self) -> String {
+        let mut result = "".to_string();
+
+        for cell in &self.cells {
+            result = result + &cell.value.to_string();
+        }
+
+        return result;
+    }
+
+    fn mark_cell(&mut self, coord: &Coord, value: u8) {
         let index = coord_to_index(coord);
+        self.mark_cell_pos(index, value);
+    }
+    
+    fn mark_cell_pos(&mut self, index: usize, value: u8) {
         let cell = self.cells.get_mut(index).unwrap();
         cell.value = value;
     }
@@ -153,7 +208,7 @@ impl SudokuBoard {
         cell.neighbors.clone()
     }
 
-    fn neighbor_values(&self, coord: &Coord) -> HashSet<u64> {
+    fn neighbor_values(&self, coord: &Coord) -> HashSet<u8> {
         let mut values = HashSet::new();
 
         for ref coord in self.neighbors(coord) {
@@ -165,10 +220,36 @@ impl SudokuBoard {
     }
 }
 
-
-
 fn main() {
-    let mut board = SudokuBoard::new();
-    board.fill();
-    board.print();
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} board_count [--all-neighbors]", args[0]);
+        return;
+    }
+
+    let board_count: usize = args[1].parse().unwrap();
+    let mut all_neighbors = false;
+    
+    if args.len() > 2 {
+        all_neighbors = args[2] == "--all-neighbors";
+    }
+
+
+    let mut board = SudokuBoard::new(all_neighbors);
+    let mut result = "".to_string();
+
+    if(all_neighbors) {
+        for n in 0..board_count {
+            board.fill();
+            result = result + &board.serialize() + "\n";
+            board.clear();
+        }
+    } else {
+        for n in 0..board_count {
+            board.fill();
+            result = result + &board.serialize() + "\n";
+        }
+    }
+
+    println!("{}", result);
 }
